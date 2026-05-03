9e0eed329da009c03cb2dfceb1f4ae723f4a8b67
AxCooking <alexander.fross@gmx.net>
2026-05-03 11:46:13 +0200

    #2 Token Hardening: GH_PUBLIC_TOKEN to null (no longer used)

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const GH_OWNER = 'AxCooking';
 const GH_REPO = 'AxCooking-Test';
 const GH_BRANCH = 'main';
 const GH_API = 'https://api.github.com';
-const GH_PUBLIC_TOKEN = ['gi','thub_','pat_11C','A2YSEQ08yRq75v1X1jx','_8TAdo74XckmQt1131hFPW','w4cOLQCGUCOlVhLHmhZhW7','USMCCUKF11pLnuIV'].join('');
+const GH_PUBLIC_TOKEN = null;
